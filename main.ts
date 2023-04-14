719b4e898d5b8c7d779ee4b3bc2902e0dfe29019
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2023-04-14 10:58:34 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=#404040 weight=0 icon="\uf013" block="Control"
+namespace blockControl {
+    //% block="do nothing"
+    export function test(): void {
+
+    }
+}
